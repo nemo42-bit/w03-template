690d310b3c3559da30a2ad917754abecf44c13ef
nemo42-bit <ge25meg@mytum.de>
2025-05-15 13:12:32 +0200

    implement changes

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,10 +2,10 @@
     import { onMount } from "svelte";
     import "../app.css";
     import Icon from "@iconify/svelte";
-    import { env } from '$env/dynamic/public';
+    import { env } from "$env/dynamic/public";
 
-    import FoodCard from './FoodCard.svelte';
-    import type { Meal } from '$lib/types';
+    import FoodCard from "./FoodCard.svelte";
+    import type { Meal } from "$lib/types";
 
     // Use environment variable for API URL with fallback to localhost
     let baseUrl = env.PUBLIC_API_URL || "http://localhost:8080";
@@ -13,9 +13,15 @@
     // For more information on runes and reactivity, see: https://svelte.dev/docs/svelte/what-are-runes
     let meals: Meal[] = $state([]);
 
+    async function fetchMeals() {
+        const res = await fetch(`${baseUrl}/mensa-garching/today`);
+        if (res.ok) {
+            meals = await res.json();
+        }
+    }
     // Fetch data once on component mount
     onMount(async () => {
-       // TODO Fetch meals from the API running on the baseUrl
+        await fetchMeals();
     });
 </script>
 
@@ -30,7 +36,11 @@
             <p>Loading menu items...</p>
         </div>
     {:else}
-       <!-- TODO add food-grid here -->
+        <div class="food-grid">
+            {#each meals as meal}
+                <FoodCard {meal} />
+            {/each}
+        </div>
     {/if}
 
     {#if meals.length === 0 && meals.length > 0}
